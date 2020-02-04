1f17be913dd39d27ff4cf2ae911daf574f640d95
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-02-04 09:20:17 -0500

    Added second input field

--- Quiz App/app/src/main/java/com/chigurupatiparthiv/quizapp/MainActivity.java
+++ Quiz App/app/src/main/java/com/chigurupatiparthiv/quizapp/MainActivity.java
@@ -3,12 +3,39 @@ package com.chigurupatiparthiv.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button submitButton;
+    EditText responseText;
+    TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.clickButton);
+        responseText = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Button was clicked");
+                Log.i("testButton", "This is Log.i (information)");
+                displayText.setText(String.format("%s %s", displayText.getText(), responseText.getText()));
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(responseText.getText().toString().equals(""))
+                }
+            }
+        });
     }
 }
