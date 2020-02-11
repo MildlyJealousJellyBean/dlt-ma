fba777163d6f901853140c56c8f7e3ef32cf8edf
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-02-11 10:09:50 -0500

    Created Front End

--- ButtonText/app/src/main/java/com/chigurupatiparthiv/buttontext/MainActivity.java
+++ ButtonText/app/src/main/java/com/chigurupatiparthiv/buttontext/MainActivity.java
@@ -1,6 +1,7 @@
 package com.chigurupatiparthiv.buttontext;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,3 +12,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+/*
+package com.chigurupatiparthiv.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    EditText responseText;
+    EditText gradeInt;
+    TextView displayText;
+    int incrementValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        responseText = findViewById(R.id.responseEditText);
+        gradeInt = findViewById(R.id.response2EditText);
+        displayText = findViewById(R.id.textBox);
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(responseText.getText().toString().equals(getString(R.string.name_match))) {
+                        displayText.setText(R.string.name_match_message);
+                    }
+                }
+                else {
+                    displayText.setText(R.string.center_text);
+                    responseText.setText("");
+                    responseText.setHint(R.string.hint1_text);
+                }
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void submitAction(View view) {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        String nameText = responseText.getText().toString().trim();
+        String gradeText = gradeInt.getText().toString().trim();
+        Log.i("responseText", nameText);
+        int gradeInt = Log.i("gradeInt", gradeText);
+        Log.i("clickTime", String.format("%s\n", dateFormat.format(new Date())));
+        if(nameText.equals("")) {
+            nameText = "User";
+        }
+        changeActionBarTitle(String.format("Hello %s!", nameText));
+        displayText.setText(getString(R.string.center_text) + " " + Integer.toString(++incrementValue));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                changeActionBarTitle(getString(R.string.app_name));
+            }
+        }, 3000);
+    }
+
+    public void changeActionBarTitle(String title) {
+        setTitle(title);
+    }
+}
+
+ */
