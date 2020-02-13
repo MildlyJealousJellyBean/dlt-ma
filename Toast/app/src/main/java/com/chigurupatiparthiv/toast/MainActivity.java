869ba4a30882812796ab1afcfb396dbd6bfe060d
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-02-13 10:13:51 -0500

    Finished Activity Main

--- Toast/app/src/main/java/com/chigurupatiparthiv/toast/MainActivity.java
+++ Toast/app/src/main/java/com/chigurupatiparthiv/toast/MainActivity.java
@@ -3,12 +3,20 @@ package com.chigurupatiparthiv.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    int[] textViewCounters = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        switch (view.getId()) {
+            case (R.id.)
+        }
     }
 }
