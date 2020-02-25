058a1678d9afff24a8e36566133bf8a9961f6277
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-02-25 09:49:16 -0500

    Finished Second Activity

--- Views/app/src/main/java/com/chigurupatiparthiv/views/FirstDisplay.java
+++ Views/app/src/main/java/com/chigurupatiparthiv/views/FirstDisplay.java
@@ -2,6 +2,7 @@ package com.chigurupatiparthiv.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,10 @@ public class FirstDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_display);
+        findViewById(R.id.topLeft).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.topRight).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.bottomLeft).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.bottomRight).setBackgroundColor(Color.GRAY);
     }
 
     public void goBack(View view) {
--- Views/app/src/main/java/com/chigurupatiparthiv/views/SecondDisplay.java
+++ Views/app/src/main/java/com/chigurupatiparthiv/views/SecondDisplay.java
@@ -2,6 +2,7 @@ package com.chigurupatiparthiv.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,9 @@ public class SecondDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_display);
+        findViewById(R.id.topMiddle).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.middleRight).setBackgroundColor(Color.GRAY);
+        findViewById(R.id.middleLeft).setBackgroundColor(Color.GRAY);
     }
 
     public void goBack(View view) {
