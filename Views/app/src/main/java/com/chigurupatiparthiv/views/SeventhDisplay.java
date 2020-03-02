dc2df9defa4d41d543fe5eed268ca3545b529e6e
Parthiv Chigurupati <2020pchiguru@tjhsst.edu>
2020-03-02 08:19:59 -0500

    Started 7th View

--- Views/app/src/main/java/com/chigurupatiparthiv/views/SeventhDisplay.java
+++ Views/app/src/main/java/com/chigurupatiparthiv/views/SeventhDisplay.java
@@ -2,6 +2,7 @@ package com.chigurupatiparthiv.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,7 @@ public class SeventhDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seventh_display);
+        findViewById(R.id.topMiddle2).setBackgroundColor(Color.GRAY);
     }
 
     public void goBack(View view) {
